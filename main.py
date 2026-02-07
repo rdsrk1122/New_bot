1d9dc90309ba1e700d52430ac55dde7e7308481f
rdsrk1122 <mdrakibdewan488@gmail.com>
2026-02-07 19:59:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-aimport time
+import time
 import json
 import telebot
 
@@ -12,7 +12,7 @@ CHANNELS = ["@amrrdsteam"] #add channels to be checked here in the format - ["Ch
               #you can add as many channels here and also add the '@' sign before channel username
 Daily_bonus = 1 #Put daily bonus amount here!
 Mini_Withdraw = 0.5  #remove 0 and add the minimum withdraw u want to set
-Per_Refer = 0.0001 #add per refer bonus here
+Per_Refer = 50.0 #add per refer bonus here
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
